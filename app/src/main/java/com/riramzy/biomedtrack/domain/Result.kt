9bad14cad8cfa61dd224f7cefdb6dbbdabd617ad
riramzy <ramzyi081@gmail.com>
2026-03-17 16:06:06 +0200

    Created a Result.kt to handle any task state, and it's for my all domain layer operations

---
+++ app/src/main/java/com/riramzy/biomedtrack/domain/Result.kt
@@ -0,0 +1,7 @@
+package com.riramzy.biomedtrack.domain
+
+sealed class Result<out T> {
+    data class Success<T>(val data: T): Result<T>()
+    data class Error(val message: String,val cause: Throwable? = null): Result<Nothing>()
+    data object Loading: Result<Nothing>()
+}
