0b1444442aed58603f5207c5c34b2a2c4d34fb96
riramzy <ramzyi081@gmail.com>
2026-03-17 17:37:43 +0200

    Created all domain repository interfaces

---
+++ app/src/main/java/com/riramzy/biomedtrack/domain/repo/AuthRepo.kt
@@ -0,0 +1,18 @@
+package com.riramzy.biomedtrack.domain.repo
+
+import com.riramzy.biomedtrack.domain.model.Department
+import com.riramzy.biomedtrack.domain.model.Technician
+import com.riramzy.biomedtrack.domain.model.UserRole
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepo {
+    suspend fun login(email: String, password: String): Result<Technician>
+    suspend fun logout(): Result<Unit>
+    suspend fun getCurrentUser(): Technician?
+    suspend fun createUser(technician: Technician): Result<Technician>
+    fun getAllUsers(): Flow<List<Technician>>
+    suspend fun updateUserRole(userId: String, role: UserRole): Result<Unit>
+    suspend fun updateUserDepartments(userId: String, departments: List<Department>): Result<Unit>
+    suspend fun deactivateUser(userId: String): Result<Unit>
+    suspend fun activateUser(userId: String): Result<Unit>
+}
---
+++ app/src/main/java/com/riramzy/biomedtrack/domain/repo/EquipmentRepo.kt
@@ -0,0 +1,16 @@
+package com.riramzy.biomedtrack.domain.repo
+
+import com.riramzy.biomedtrack.domain.model.Department
+import com.riramzy.biomedtrack.domain.model.Equipment
+import kotlinx.coroutines.flow.Flow
+
+interface EquipmentRepo {
+    fun getAllEquipment(): Flow<List<Equipment>>
+    fun getEquipmentByDepartment(department: Department): Flow<List<Equipment>>
+    suspend fun getEquipmentById(id: String): Equipment?
+    suspend fun getEquipmentCount(): Triple<Int, Int, Int>
+    suspend fun addEquipment(equipment: Equipment): Result<Unit>
+    suspend fun updateEquipment(equipment: Equipment): Result<Unit>
+    suspend fun deleteEquipment(id: String): Result<Unit>
+}
+
