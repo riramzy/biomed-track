ab81eebe87b58443279f56e91b31b1b2c7159fd8
riramzy <ramzyi081@gmail.com>
2026-03-19 20:55:08 +0200

    Created Firestore collection constants

---
+++ app/src/main/java/com/riramzy/biomedtrack/data/remote/firebase/FirebaseCollections.kt
@@ -0,0 +1,9 @@
+package com.riramzy.biomedtrack.data.remote.firebase
+
+object FirebaseCollections {
+    const val EQUIPMENT = "equipment"
+    const val MAINTENANCE_LOGS = "maintenanceLogs"
+    const val TECHNICIANS = "users"
+    const val TASKS = "tasks"
+    const val STATUS_CHANGE_LOGS = "statusChangeLogs"
+}
