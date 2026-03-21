6b315d47ecf3c935686e4d9d9b1f9b378991e130
riramzy <ramzyi081@gmail.com>
2026-03-21 17:05:29 +0200

    Created the bottom navigation bar and the insight card

--- app/src/main/java/com/riramzy/biomedtrack/ui/theme/Theme.kt
+++ app/src/main/java/com/riramzy/biomedtrack/ui/theme/Theme.kt
@@ -8,7 +8,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
@@ -252,11 +255,25 @@ val unspecified_scheme = ColorFamily(
     Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
 )
 
+@Immutable
+data class IndicatorColors(
+    val red: Color = Color.Unspecified,
+    val green: Color = Color.Unspecified,
+    val yellow: Color = Color.Unspecified
+)
+
+val LocalIndicatorColors = staticCompositionLocalOf { IndicatorColors() }
+
+val MaterialTheme.indicatorColors: IndicatorColors
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalIndicatorColors.current
+
 @Composable
-fun AppTheme(
+fun BioMedTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
@@ -269,10 +286,17 @@ fun AppTheme(
       else -> lightScheme
   }
 
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = AppTypography,
-    content = content
+  val indicatorColors = IndicatorColors(
+      red = redIndicatorLight,
+      green = greenIndicatorLight,
+      yellow = yellowIndicatorLight
   )
-}
 
+  CompositionLocalProvider(LocalIndicatorColors provides indicatorColors) {
+      MaterialTheme(
+          colorScheme = colorScheme,
+          typography = AppTypography,
+          content = content
+      )
+  }
+}
