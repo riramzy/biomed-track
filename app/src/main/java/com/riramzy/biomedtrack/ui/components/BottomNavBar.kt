ce895919550188f3c18c87d6181de846ab8c7abd
riramzy <ramzyi081@gmail.com>
2026-03-21 11:22:10 +0200

    Created my screens skeletons

---
+++ app/src/main/java/com/riramzy/biomedtrack/ui/components/BottomNavBar.kt
@@ -0,0 +1,2 @@
+package com.riramzy.biomedtrack.ui.components
+
---
+++ app/src/main/java/com/riramzy/biomedtrack/ui/screens/equipment/EditEquipmentScreen.kt
@@ -0,0 +1,19 @@
+package com.riramzy.biomedtrack.ui.screens.equipment
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun EditEquipmentScreen(navController: NavHostController, equipmentId: String?) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Edit Equipment Screen")
+    }
+}
