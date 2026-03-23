6987a0383e68578edcbe253bca77a0482230cd18
riramzy <ramzyi081@gmail.com>
2026-03-23 22:54:45 +0200

    Created ActivityCard and StatusIndicator components

---
+++ app/src/main/java/com/riramzy/biomedtrack/ui/components/BioMedActivityCard.kt
@@ -0,0 +1,213 @@
+package com.riramzy.biomedtrack.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.riramzy.biomedtrack.R
+import com.riramzy.biomedtrack.ui.theme.BioMedTheme
+import com.riramzy.biomedtrack.ui.theme.indicatorColors
+
+@Composable
+fun BioMedActivityCard() {
+    Card(
+        modifier = Modifier
+            .width(386.dp)
+            .height(155.dp),
+        shape = RoundedCornerShape(25.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = if (isSystemInDarkTheme()) {
+                MaterialTheme.colorScheme.onSecondary
+            } else {
+                MaterialTheme.colorScheme.primaryContainer.copy(0.3f)
+            }
+        )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 15.dp,
+                        end = 15.dp,
+                        top = 10.dp,
+                        bottom = 10.dp
+                    ),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(34.dp),
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.indicatorColors.green
+                    )
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.activity_online),
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+
+                Column(
+                    modifier = Modifier
+                        .weight(2f)
+                        .padding(start = 15.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Status Changed to Online",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.labelSmall,
+                        color = if (isSystemInDarkTheme()) {
+                            Color.White
+                        } else {
+                            Color.Black
+                        }
+                    )
+
+                    Text(
+                        text = "Fresenius 4008S - 4545885N70",
+                        fontSize = 11.sp,
+                        fontWeight = FontWeight.Normal,
+                        style = MaterialTheme.typography.labelSmall,
+                        color = if (isSystemInDarkTheme()) {
+                            Color.White
+                        } else {
+                            Color.Black
+                        }
+                    )
+
+                    BioMedStatusIndicator(modifier = Modifier.padding(top = 5.dp))
+
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier.padding(top = 5.dp)
+                    ) {
+                        Text(
+                            text = "Department",
+                            fontSize = 11.sp,
+                            fontWeight = FontWeight.Light,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+
+                        Text(
+                            text = "Dialysis Unit",
+                            fontSize = 11.sp,
+                            fontWeight = FontWeight.Medium,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = if (isSystemInDarkTheme()) {
+                                Color.White
+                            } else {
+                                Color.Black
+                            }
+                        )
+                    }
+
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier.padding(top = 5.dp)
+                    ) {
+                        Text(
+                            text = "By",
+                            fontSize = 11.sp,
+                            fontWeight = FontWeight.Light,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+
+                        Text(
+                            text = "Ramsey Ibrahim",
+                            fontSize = 11.sp,
+                            fontWeight = FontWeight.Medium,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = if (isSystemInDarkTheme()) {
+                                Color.White
+                            } else {
+                                Color.Black
+                            }
+                        )
+                    }
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Text(
+                        text = "12m ago",
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Normal,
+                        style = MaterialTheme.typography.labelSmall,
+                        textAlign = TextAlign.End,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Text(
+                        text = "13/3/2026",
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Medium,
+                        style = MaterialTheme.typography.labelSmall,
+                        textAlign = TextAlign.End,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(device = "spec:width=411dp,height=891dp", showBackground = true)
+@Composable
+fun BioMedActivityCardPreview() {
+    BioMedTheme {
+        BioMedActivityCard()
+    }
+}
+
+@Preview(device = "spec:width=411dp,height=891dp", showBackground = true, showSystemUi = false,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun BioMedActivityCardDarkPreview() {
+    BioMedTheme {
+        BioMedActivityCard()
+    }
+}
