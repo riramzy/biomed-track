62807eafc7fe97e3e083e059bce5b9fbf4f918d5
riramzy <ramzyi081@gmail.com>
2026-03-24 02:28:07 +0200

    Created Department Insight card and Equipment Status Card

---
+++ app/src/main/java/com/riramzy/biomedtrack/ui/components/BioMedEquipmentStatusCard.kt
@@ -0,0 +1,197 @@
+package com.riramzy.biomedtrack.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.riramzy.biomedtrack.R
+import com.riramzy.biomedtrack.ui.theme.BioMedTheme
+import com.riramzy.biomedtrack.ui.theme.indicatorColors
+
+@Composable
+fun BioMedEquipmentStatus() {
+    Card(
+        modifier = Modifier
+            .width(380.dp)
+            .height(90.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = if (isSystemInDarkTheme()) {
+                MaterialTheme.colorScheme.onSecondary
+            } else {
+                MaterialTheme.colorScheme.primaryContainer.copy(0.3f)
+            }
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(35.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.indicatorColors.red),
+            )
+
+            Column(
+                modifier = Modifier
+                    .weight(2f)
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Fresenius 4008S",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.labelSmall,
+                    color = if (isSystemInDarkTheme()) {
+                        Color.White
+                    } else {
+                        Color.Black
+                    }
+
+                )
+                Text(
+                    text = "4545885N70",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Medium,
+                    style = MaterialTheme.typography.labelSmall,
+                    color = if (isSystemInDarkTheme()) {
+                        Color.White
+                    } else {
+                        Color.Black
+                    }
+                )
+                Text(
+                    text = "Dialysis Unit",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Light,
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(end = 15.dp),
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "20/3/2026",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Medium,
+                    style = MaterialTheme.typography.labelSmall,
+                    color = if (isSystemInDarkTheme()) {
+                        Color.White
+                    } else {
+                        Color.Black
+                    }
+                )
+
+                Text(
+                    text = "Hemodialysis",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Normal,
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+
+                LogMaintenancePill(
+                    modifier = Modifier.padding(top = 5.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LogMaintenancePill(
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .width(69.dp)
+            .height(19.dp),
+        shape = CircleShape,
+        colors = CardDefaults.cardColors(
+            containerColor = if (isSystemInDarkTheme()) {
+                Color.White
+            } else {
+                Color.Black
+            }
+        )
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .padding(start = 4.dp)
+                    .size(12.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.activity_log),
+                    contentDescription = null,
+                    tint = if (isSystemInDarkTheme()) {
+                        Color.Black
+                    } else {
+                        Color.White
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview(device = "id:pixel_9", showBackground = true)
+@Composable
+fun BioMedEquipmentStatusPreview() {
+    BioMedTheme {
+        BioMedEquipmentStatus()
+    }
+}
+
+@Preview(device = "id:pixel_9", showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun BioMedEquipmentStatusDarkPreview() {
+    BioMedTheme {
+        BioMedEquipmentStatus()
+    }
+}
