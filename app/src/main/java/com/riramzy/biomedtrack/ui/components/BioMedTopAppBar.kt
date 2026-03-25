cbffdae39cf5a58f491bc52492a64fd8a5b01cd7
riramzy <ramzyi081@gmail.com>
2026-03-25 21:20:15 +0200

    Created my app's top app bar

---
+++ app/src/main/java/com/riramzy/biomedtrack/ui/components/BioMedTopAppBar.kt
@@ -0,0 +1,140 @@
+package com.riramzy.biomedtrack.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.riramzy.biomedtrack.R
+import com.riramzy.biomedtrack.ui.theme.BioMedTheme
+
+@Composable
+fun BioMedTopAppBar(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Card(
+            modifier = Modifier
+                .width(370.dp)
+                .height(45.dp),
+            shape = RoundedCornerShape(25.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = if (isSystemInDarkTheme()) {
+                    MaterialTheme.colorScheme.onSecondary
+                } else {
+                    MaterialTheme.colorScheme.primaryContainer
+                }
+            )
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 6.dp)
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.biomedtrack),
+                    contentDescription = null,
+                    modifier = Modifier.size(35.dp),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(6.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.size(35.dp),
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = if (isSystemInDarkTheme()) {
+                                MaterialTheme.colorScheme.secondaryContainer
+                            } else {
+                                MaterialTheme.colorScheme.primary
+                            }
+                        )
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.notifications),
+                            contentDescription = null,
+                            modifier = Modifier.size(25.dp),
+                            tint = if (isSystemInDarkTheme()) {
+                                MaterialTheme.colorScheme.primary
+                            } else {
+                                Color.White
+                            }
+                        )
+                    }
+
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.size(35.dp),
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = if (isSystemInDarkTheme()) {
+                                MaterialTheme.colorScheme.secondaryContainer
+                            } else {
+                                MaterialTheme.colorScheme.primary
+                            }
+                        )
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.profile_picture),
+                            contentDescription = null,
+                            modifier = Modifier.size(25.dp),
+                            tint = if (isSystemInDarkTheme()) {
+                                MaterialTheme.colorScheme.primary
+                            } else {
+                                Color.White
+                            },
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(device = "id:pixel_9", showBackground = true)
+@Composable
+fun BioMedTopAppBarPreview() {
+    BioMedTheme {
+        BioMedTopAppBar()
+    }
+}
+
+@Preview(device = "id:pixel_9", showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun BioMedTopAppBarDarkPreview() {
+    BioMedTheme {
+        BioMedTopAppBar()
+    }
+}
+
