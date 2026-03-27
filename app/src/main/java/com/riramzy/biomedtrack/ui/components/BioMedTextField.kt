a4b0deb978b5f15057f1205eb627da82c7afe688
riramzy <ramzyi081@gmail.com>
2026-03-27 15:56:28 +0200

    Created the app's custom text field

---
+++ app/src/main/java/com/riramzy/biomedtrack/ui/components/BioMedTextField.kt
@@ -0,0 +1,207 @@
+package com.riramzy.biomedtrack.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.riramzy.biomedtrack.ui.theme.BioMedTheme
+
+@Composable
+fun BioMedTextField(
+    modifier: Modifier = Modifier,
+    title: String = "By",
+    isNoteCard: Boolean = true
+) {
+    var text by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(
+                start = 20.dp,
+                end = 20.dp,
+                bottom = 15.dp
+            )
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.labelLarge,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            color = if (isSystemInDarkTheme()) {
+                MaterialTheme.colorScheme.onSecondaryContainer
+            } else {
+                MaterialTheme.colorScheme.onPrimaryContainer
+            }
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+        ) {
+            Card(
+                modifier = Modifier
+                    .width(328.dp)
+                    .height(
+                        if (isNoteCard) {
+                            93.dp
+                        } else {
+                            45.dp
+                        }
+                    ),
+                shape = RoundedCornerShape(20.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = if (isSystemInDarkTheme()) {
+                        MaterialTheme.colorScheme.secondaryContainer
+                    } else {
+                        MaterialTheme.colorScheme.primaryContainer.copy(0.4f)
+                    }
+                ),
+                border =  if (text.isEmpty()) {
+                    BorderStroke(
+                        width = 1.dp,
+                        color = if (isSystemInDarkTheme()) {
+                            MaterialTheme.colorScheme.secondary
+                        } else {
+                            MaterialTheme.colorScheme.primaryContainer
+                        }
+                    )
+                } else {
+                    BorderStroke(
+                        width = 1.dp,
+                        color = if (isSystemInDarkTheme()) {
+                            MaterialTheme.colorScheme.secondaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.primary
+                        }
+                    )
+                }
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    BasicTextField(
+                        value = text,
+                        onValueChange = {
+                            text = it
+                        },
+                        singleLine = if (isNoteCard) {
+                            false
+                        } else {
+                            true
+                        },
+                        textStyle = TextStyle.Default.copy(
+                            color = if (isSystemInDarkTheme()) {
+                                Color.White
+                            } else {
+                                Color.Black
+                            }
+                        ),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        cursorBrush = SolidColor(
+                            if (isSystemInDarkTheme()) {
+                                MaterialTheme.colorScheme.primary
+                            } else {
+                                MaterialTheme.colorScheme.primary
+                            }
+                        ),
+                        decorationBox = { innerTextField ->
+                            Box(
+                                modifier = Modifier
+                                    .height(
+                                        if (isNoteCard) {
+                                            93.dp
+                                        } else {
+                                            45.dp
+                                        }
+                                    )
+                                    .fillMaxWidth()
+                                    .background(
+                                        color = Color.Unspecified,
+                                        shape = RoundedCornerShape(8.dp)
+                                    )
+                                    .padding(horizontal = 6.dp, vertical = 0.dp),
+                                contentAlignment = if (isNoteCard) {
+                                    Alignment.TopStart
+                                } else {
+                                    Alignment.CenterStart
+                                }
+                            ) {
+                                if (text.isEmpty()) {
+                                    Text(
+                                        text = if (isNoteCard) {
+                                            "Describe the work performed, issues found,parts replaced, etc"
+                                        } else {
+                                            "Enter name"
+                                        },
+                                        style = MaterialTheme.typography.labelLarge,
+                                        color = MaterialTheme.colorScheme.secondary,
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Normal,
+                                    )
+                                }
+
+                                innerTextField()
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(device = "id:pixel_9", showBackground = true)
+@Composable
+fun BioMedTextFieldPreview() {
+    BioMedTheme {
+        BioMedTextField()
+    }
+}
+
+@Preview(device = "id:pixel_9", showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun BioMedTextFieldDarkPreview() {
+    BioMedTheme {
+        BioMedTextField()
+    }
+}
