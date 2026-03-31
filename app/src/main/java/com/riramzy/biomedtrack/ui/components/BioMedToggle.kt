34ef3df195bf22081a1e150b5e634609014e9825
riramzy <ramzyi081@gmail.com>
2026-03-31 15:29:47 +0200

    Created the app's custom toggle button

---
+++ app/src/main/java/com/riramzy/biomedtrack/ui/components/BioMedToggle.kt
@@ -0,0 +1,109 @@
+package com.riramzy.biomedtrack.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.riramzy.biomedtrack.ui.theme.BioMedTheme
+
+@Composable
+fun BioMedToggle(
+    modifier: Modifier = Modifier,
+    text: String = "Toggle",
+    isChecked: Boolean = true
+) {
+    Card(
+        modifier = modifier
+            .height(35.dp),
+        shape = RoundedCornerShape(25.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = if (isSystemInDarkTheme()) {
+                if (isChecked) {
+                    MaterialTheme.colorScheme.secondaryContainer
+                } else {
+                    MaterialTheme.colorScheme.secondaryContainer.copy(0.5f)
+                }
+            } else {
+                if (isChecked) {
+                    MaterialTheme.colorScheme.primaryContainer
+                } else {
+                    MaterialTheme.colorScheme.primaryContainer.copy(0.5f)
+                }
+            }
+        )
+    ) {
+        Row(
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.labelLarge,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .padding(horizontal = 15.dp),
+                color = if (isSystemInDarkTheme()) {
+                    MaterialTheme.colorScheme.primary
+                } else {
+                    MaterialTheme.colorScheme.primary
+                }
+            )
+
+            Switch(
+                checked = isChecked,
+                onCheckedChange = {},
+                modifier = Modifier
+                    .padding(vertical = 6.dp, horizontal = 2.dp),
+                colors = SwitchDefaults.colors(
+                    uncheckedBorderColor = if (isSystemInDarkTheme()) {
+                        MaterialTheme.colorScheme.primary
+                    } else {
+                        MaterialTheme.colorScheme.primary
+                    },
+                    uncheckedThumbColor = if (isSystemInDarkTheme()) {
+                        MaterialTheme.colorScheme.primary
+                    } else {
+                        MaterialTheme.colorScheme.primary
+                    }
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false, device = "id:pixel_9")
+@Composable
+fun BioMedTogglePreview() {
+    BioMedTheme {
+        BioMedToggle()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false, device = "id:pixel_9",
+    backgroundColor = 0xFF000000,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun BioMedToggleDarkPreview() {
+    BioMedTheme {
+        BioMedToggle()
+    }
+}
