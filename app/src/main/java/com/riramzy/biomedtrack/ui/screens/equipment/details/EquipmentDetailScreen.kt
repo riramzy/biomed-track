a11f3a35bc26f887b22dc07c597b2341a87fbbe3
riramzy <ramzyi081@gmail.com>
2026-04-16 21:44:16 +0200

    Finished the EquipmentDetailScreen.kt, EquipmentDetailsVm and Shimmer screen for loading states

--- app/src/main/java/com/riramzy/biomedtrack/ui/screens/equipment/details/EquipmentDetailScreen.kt
+++ app/src/main/java/com/riramzy/biomedtrack/ui/screens/equipment/details/EquipmentDetailScreen.kt
@@ -1,4 +1,4 @@
-package com.riramzy.biomedtrack.ui.screens.equipment
+package com.riramzy.biomedtrack.ui.screens.equipment.details
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
