c4ec570bee0fd884077df0234e4801d127898e20
riramzy <ramzyi081@gmail.com>
2026-04-24 21:06:40 +0300

    Created a data class to hold my data for the activity card and notifications cards across the app

---
+++ app/src/main/java/com/riramzy/biomedtrack/utils/ActivityItem.kt
@@ -0,0 +1,17 @@
+package com.riramzy.biomedtrack.utils
+
+data class ActivityItem(
+    val id: String,
+    val type: ActivityType,
+    val title: String,
+    val equipmentName: String,
+    val equipmentModel: String,
+    val equipmentSerial: String,
+    val departmentName: String,
+    val technicianName: String,
+    val timestamp: Long,
+    val dueDate: String? = null,
+    val status: String,
+    val isRead: Boolean = false
+)
+
