15cb5d47c90b1599c934d4bc5314a8f0f65703da
riramzy <ramzyi081@gmail.com>
2026-04-26 09:10:14 +0300

    Updated Repo and RepoImpl with a markAsRead() that updates my readBy list and add the current user's ID with that exact log or tasks ID, and Added a getAllMaintenanceLogs() to get a flow of all MaintenanceLogs to use across the app for fetching and displaying Logs in realtime

--- app/src/main/java/com/riramzy/biomedtrack/domain/repo/TaskRepo.kt
+++ app/src/main/java/com/riramzy/biomedtrack/domain/repo/TaskRepo.kt
@@ -2,7 +2,7 @@ package com.riramzy.biomedtrack.domain.repo
 
 import com.riramzy.biomedtrack.domain.model.Task
 import kotlinx.coroutines.flow.Flow
-import com.riramzy.biomedtrack.domain.Result
+import com.riramzy.biomedtrack.utils.Result
 
 interface TaskRepo {
     fun getTasksForTechnician(technicianId: String): Flow<List<Task>>
@@ -10,4 +10,5 @@ interface TaskRepo {
     suspend fun addTask(task: Task): Result<Unit>
     suspend fun updateTask(task: Task): Result<Unit>
     suspend fun deleteTask(taskId: String): Result<Unit>
+    suspend fun markAsRead(logId: String, userId: String): Result<Unit>
 }
