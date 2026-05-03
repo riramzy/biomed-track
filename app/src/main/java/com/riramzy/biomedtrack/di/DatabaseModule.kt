6bb9aed00aca45edd8e5be0a2891ad4355338378
riramzy <ramzyi081@gmail.com>
2026-05-03 08:33:55 +0300

    Refactored Room converters to use injected Gson, added support for String list and Department serialization, and registered new repository and DAO bindings

--- app/src/main/java/com/riramzy/biomedtrack/di/DatabaseModule.kt
+++ app/src/main/java/com/riramzy/biomedtrack/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.riramzy.biomedtrack.di
 
 import android.content.Context
 import androidx.room.Room
+import com.google.gson.Gson
 import com.riramzy.biomedtrack.data.local.database.BioMedDatabase
 import com.riramzy.biomedtrack.data.local.database.RoomConverters
 import dagger.Module
@@ -16,18 +17,34 @@ import javax.inject.Singleton
 class DatabaseModule {
     @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext context: Context): BioMedDatabase {
+    fun providesGson(): Gson = Gson()
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context,
+        converters: RoomConverters
+    ): BioMedDatabase {
         return Room.databaseBuilder(
             context,
             BioMedDatabase::class.java,
             "biomed_database"
         )
-            .addTypeConverter(RoomConverters())
+            .addTypeConverter(converters)
+            .fallbackToDestructiveMigration(true)
             .build()
     }
 
     @Provides
+    fun provideRoomConverters(gson: Gson): RoomConverters {
+        return RoomConverters(gson)
+    }
+
+    @Provides
     fun provideEquipmentDao(database: BioMedDatabase) = database.equipmentDao()
+
+    @Provides
+    fun provideDepartmentDao(database: BioMedDatabase) = database.departmentDao()
 
     @Provides
     fun provideMaintenanceLogDao(database: BioMedDatabase) = database.maintenanceLogDao()
