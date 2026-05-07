fef3aeb6e6a4696f96762ee83e07c76b57cc4507
riramzy <ramzyi081@gmail.com>
2026-05-07 23:08:15 +0300

    Updated the Room database to include the Department entity and DAO, and bumped the database version to 8

--- app/src/main/java/com/riramzy/biomedtrack/data/local/database/BioMedDatabase.kt
+++ app/src/main/java/com/riramzy/biomedtrack/data/local/database/BioMedDatabase.kt
@@ -3,11 +3,13 @@ package com.riramzy.biomedtrack.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.riramzy.biomedtrack.data.local.dao.DepartmentDao
 import com.riramzy.biomedtrack.data.local.dao.EquipmentDao
 import com.riramzy.biomedtrack.data.local.dao.MaintenanceLogDao
 import com.riramzy.biomedtrack.data.local.dao.StatusChangeLogDao
 import com.riramzy.biomedtrack.data.local.dao.TaskDao
 import com.riramzy.biomedtrack.data.local.dao.TechnicianDao
+import com.riramzy.biomedtrack.data.local.entity.DepartmentEntity
 import com.riramzy.biomedtrack.data.local.entity.EquipmentEntity
 import com.riramzy.biomedtrack.data.local.entity.MaintenanceLogEntity
 import com.riramzy.biomedtrack.data.local.entity.StatusChangeLogEntity
@@ -17,17 +19,19 @@ import com.riramzy.biomedtrack.data.local.entity.TechnicianEntity
 @Database(
     entities = [
         EquipmentEntity::class,
+        DepartmentEntity::class,
         MaintenanceLogEntity::class,
         StatusChangeLogEntity::class,
         TaskEntity::class,
         TechnicianEntity::class
     ],
-    version = 1,
+    version = 8,
     exportSchema = false
 )
 @TypeConverters(RoomConverters::class)
 abstract class BioMedDatabase: RoomDatabase() {
     abstract fun equipmentDao(): EquipmentDao
+    abstract fun departmentDao(): DepartmentDao
     abstract fun maintenanceLogDao(): MaintenanceLogDao
     abstract fun statusChangeLogDao(): StatusChangeLogDao
     abstract fun taskDao(): TaskDao
