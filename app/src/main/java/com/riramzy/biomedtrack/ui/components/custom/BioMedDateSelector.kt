883ea0b63a54f113d8d6ad66faa54d7ace1b3a7b
riramzy <ramzyi081@gmail.com>
2026-05-22 22:15:27 +0300

    Updated BioMedDateSelector to handle dates as Long timestamps, integrated the toDateString utility for formatting, and introduced the BioMedDateRangeSelector component for selecting custom date ranges via an AlertDialog

--- app/src/main/java/com/riramzy/biomedtrack/ui/components/custom/BioMedDateSelector.kt
+++ app/src/main/java/com/riramzy/biomedtrack/ui/components/custom/BioMedDateSelector.kt
@@ -6,16 +6,13 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DatePicker
@@ -28,6 +25,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -40,17 +38,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.riramzy.biomedtrack.R
 import com.riramzy.biomedtrack.ui.theme.BioMedTheme
-import java.time.Instant
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
+import com.riramzy.biomedtrack.utils.Timestamps.toDateString
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BioMedDateSelector(
     modifier: Modifier = Modifier,
     title: String = "Date",
-    selectedDate: String = "Select Date",
-    onDateSelected: (String) -> Unit = {}
+    selectedDate: Long = System.currentTimeMillis(),
+    onDateSelected: (Long) -> Unit = {}
 ) {
     var showDatePicker by remember { mutableStateOf(false) }
     val datePickerState = rememberDatePickerState()
@@ -109,7 +105,7 @@ fun BioMedDateSelector(
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
-                        text = selectedDate.ifEmpty { "Select Date" },
+                        text = selectedDate.toDateString(),
                         style = MaterialTheme.typography.labelLarge,
                         fontSize = 14.sp,
                         fontWeight = FontWeight.Normal
@@ -132,11 +128,7 @@ fun BioMedDateSelector(
                     confirmButton = {
                         TextButton(
                             onClick = {
-                                datePickerState.selectedDateMillis?.let {
-                                    val date = Instant.ofEpochMilli(it)
-                                        .atZone(ZoneId.systemDefault())
-                                        .toLocalDate()
-                                        .format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                                datePickerState.selectedDateMillis?.let { date ->
                                     onDateSelected(date)
                                 }
                                 showDatePicker = false
@@ -164,11 +156,68 @@ fun BioMedDateSelector(
     }
 }
 
+@Composable
+fun BioMedDateRangeSelector(
+    modifier: Modifier = Modifier,
+    startDate: Long = System.currentTimeMillis() - 7 * 24 * 60 * 60 * 1000,
+    endDate: Long = System.currentTimeMillis(),
+    onConfirm: (Long, Long) -> Unit = { _, _ -> },
+    onCancel: () -> Unit = {}
+) {
+    var selectedStartDate by remember { mutableLongStateOf(startDate) }
+    var selectedEndDate by remember { mutableLongStateOf(endDate) }
+
+    AlertDialog(
+        onDismissRequest = onCancel,
+        title = { Text("Select Custom Date Range") },
+        text = {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                BioMedDateSelector(
+                    title = "Start Date",
+                    selectedDate = selectedStartDate,
+                    onDateSelected = {
+                        selectedStartDate = it
+                    },
+                    modifier = Modifier
+                )
+
+                BioMedDateSelector(
+                    title = "End Date",
+                    selectedDate = selectedEndDate,
+                    onDateSelected = {
+                        selectedEndDate = it
+                    },
+                    modifier = Modifier
+                )
+            }
+        },
+        confirmButton = {
+            BioMedButton(
+                text = "Confirm",
+                onClick = {
+                    onConfirm(selectedStartDate, selectedEndDate)
+                },
+                customColor = MaterialTheme.colorScheme.primary,
+                customTextColor = MaterialTheme.colorScheme.onPrimary
+            )
+        },
+        dismissButton = {
+            BioMedButton(
+                text = "Cancel",
+                onClick = onCancel
+            )
+        },
+        modifier = modifier
+    )
+}
+
 @Preview(device = "id:pixel_9", showBackground = true)
 @Composable
 fun BioMedDateSelectorPreview() {
     BioMedTheme {
-        BioMedDateSelector()
+        BioMedDateRangeSelector()
     }
 }
 
